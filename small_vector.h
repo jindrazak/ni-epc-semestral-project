c933744575cf60c53a7b7e0a4e37cbdf31c9f8ce
Jindrich Zak <jindra.zak@gmail.com>
2020-12-02 20:25:43 +0100

    Add buf_ and start to use it in member functions.

--- small_vector.h
+++ small_vector.h
@@ -9,16 +9,22 @@ namespace epc {
     template<typename T, size_t N>
     class small_vector {
         T *data_;
+        T buf_[N];
         size_t capacity_, size_;
     public:
-        small_vector() : data_(nullptr), capacity_(0), size_(0) {}
+        small_vector() : data_(nullptr), capacity_(N), size_(0) {}
 
         small_vector(const small_vector &other) : small_vector() { // copy constructor
             if (other.size_ == 0) return;
             data_ = (T *) ::operator new(other.size_ * sizeof(T));
             capacity_ = other.size_;
-            try { std::uninitialized_copy(other.data_, other.data_ + other.size_, data_); }
-            catch (...) {
+            try {
+                if (other.capacity_ == N) {
+                    std::uninitialized_copy(other.buf_, other.buf_ + other.size_, buf_);        //todo use iterators
+                } else {
+                    std::uninitialized_copy(other.data_, other.data_ + other.size_, data_);     //todo use iterators
+                }
+            } catch (...) {
                 ::operator delete(data_);
                 throw;
             }
@@ -26,6 +32,7 @@ namespace epc {
         }
 
         small_vector(small_vector &&other) noexcept: // move constructor
+        buf_(other.buf_),
         data_(other.data_),
         capacity_(other.capacity_),
         size_(other.size_)
@@ -42,7 +49,11 @@ namespace epc {
             } else {
                 if (this != &other) {
                     clear();
-                    std::uninitialized_copy(other.data_, other.data_ + other.size_, data_);
+                    if(other.size_ <= N){
+                        std::uninitialized_copy(other.data_, other.data_ + other.size_, buf_);      //todo use iterators
+                    }else{
+                        std::uninitialized_copy(other.data_, other.data_ + other.size_, data_);     //todo use iterators
+                    }
                     size_ = other.size_;
                 }
             }
@@ -61,6 +72,7 @@ namespace epc {
         }
 
         void swap(small_vector &other) noexcept {
+            std::swap(buf_, other.buf_);
             std::swap(data_, other.data_);
             std::swap(capacity_, other.capacity_);
             std::swap(size_, other.size);
@@ -68,10 +80,16 @@ namespace epc {
 
         void reserve(size_t new_capacity){
             if (new_capacity <= capacity_) return;
+            // capacity_ is always >= N, therefore we need to use the dynamically-allocated memory
             T* data = (T*)::operator new(new_capacity * sizeof(T));
             size_t i = 0;
             try {
-                for ( ; i < size_; i++) new (data + i) T(std::move_if_noexcept(data_[i]));
+                // if capacity == N, we need to move elements from the buf_, not the data_
+                if(capacity_ == N){
+                    for ( ; i < size_; i++) new (data + i) T(std::move_if_noexcept(buf_[i]));
+                }else{
+                    for ( ; i < size_; i++) new (data + i) T(std::move_if_noexcept(data_[i]));
+                }
             } catch(...) {
                 for ( ; i > 0; i--) {
                     (data + i - 1)->~T();
@@ -96,10 +114,18 @@ namespace epc {
 
         template <typename... Ts>
         void emplace_back(Ts&&... params) {
-            if (size_ == capacity_) reserve(capacity_ == 0 ? 1 : 2 * capacity_);
-            new (data_ + size_) T(std::forward<Ts>(params)...);
-            size_++;
+            constexpr size_t numberOfParams = sizeof...(Ts);
+            if (size_ + numberOfParams > capacity_) reserve(capacity_ == 0 ? 1 : 2 * capacity_);
+            if(capacity_ == N){
+                new (buf_ + size_) T(std::forward<Ts>(params)...);
+            }else{
+                new (data_ + size_) T(std::forward<Ts>(params)...);
+            }
+            size_+= numberOfParams;
         }
+
+        int size() noexcept { return this->size_; }
+        int capacity() noexcept { return this->capacity_; }
 
         void clear() noexcept {
             try {
@@ -108,9 +134,15 @@ namespace epc {
                 }
             } catch (...) {}}
 
-        T &operator[](size_t i) { return data_[i]; }
+        T &operator[](size_t i) {
+            if(capacity_ == N) return buf_[i];
+            return data_[i];
+        }
 
-        const T &operator[](size_t i) const { return data_[i]; }
+        const T &operator[](size_t i) const {
+            if(capacity_ == N) return buf_[i];
+            return data_[i];
+        }
     };
 
     template<typename T, size_t N> void swap(small_vector<T,N> &a, small_vector<T,N> &b) noexcept { a.swap(b); }
